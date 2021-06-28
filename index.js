a707bf4c1f5d887fdeff38785076441bb1b41636
Newton <narmstrongv@gmail.com>
2021-06-27 21:13:41 -0700

    added js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var inquirer = require('inquirer');
 const fs = require('fs');
+const htmlGenerator = require('./src/html-template')
+
+const Manager = require("./lib/Manager");
+
+const teamArray = []
+//this holds team as you create them
 
 const generatePage = require('./src/html-template');
 
@@ -24,10 +30,63 @@ fs.writeFile('./dist/index.html', pageHTML, err => {
 inquirer
 .prompt([
     /* Pass your questions in here */
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+      validate: (userInput) => {
+        if (userInput !== "") {
+          return true
+        } else {
+          return "Please enter your name."
+        }
+      }
+    },
+
+    {
+    type: "input",
+    name: "id",
+    message: "What is your ID?",
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is your officeNumber?",
+    },
+
+    // {
+    //   type: "list",
+    //   name: "jobLiking",
+    //   message: "Do you like your job?",
+    //   choices: ["Yes", "No", "Not sure"],
+    //   default: "Not sure"
+     
+    // }
+
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    console.log(answers);
+    //console.log(answers.name);
+    var manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    console.log(manager);
+    teamArray.push(manager);
+    console.log("teamArray", teamArray);
+    //doing this will help you understand/remember what the console logs do
+
+    
+  }).then(() => {
+    var templateAns = htmlGenerator(teamArray);
+    console.log(templateAns);
   })
+
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
